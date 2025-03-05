0f4773912fedf7dac521c77fea60e15eff44d4bd
shogle <seyoumoon@gmail.com>
2025-03-05 21:50:02 +0900

    add RawQuery repositories.

--- repositories/test_repo.go
+++ repositories/test_repo.go
@@ -10,10 +10,10 @@ func CreateTest(db *gorm.DB, test *models.Test) error {
 	return db.Create(test).Error
 }
 
-func GetTests(db *gorm.DB) ([]models.Test, error) {
+func GetTests(db *gorm.DB) (*[]models.Test, error) {
 	var tests []models.Test
 	result := db.Find(&tests)
-	return tests, result.Error
+	return &tests, result.Error
 }
 
 func DeleteTest(db *gorm.DB, id uint) error {
@@ -24,4 +24,10 @@ func GetTestByID(db *gorm.DB, id uint) (*models.Test, error) {
 	var test models.Test
 	result := db.First(&test, id)
 	return &test, result.Error
+}
+
+func RawQuery(db *gorm.DB) (*[]models.Test, error) {
+	var testValue []models.Test
+	result := db.Raw("SELECT id, test FROM tests").Scan(&testValue) // 이것도 repo로 옮길 것
+	return &testValue, result.Error
 }
